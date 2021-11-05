8a13373646bd3808b0f3e2bf85b13bc6a834439d
JIINHEO <gjwldls123@gmail.com>
2021-11-05 20:06:11 +0900

    First commit

---
+++ ios-timer-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-timer-app
+//
+//  Created by 허지인 on 2021/11/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
